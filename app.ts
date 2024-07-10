c8e2ae9d347a57082887aa9ff66ff423980e489f
LeshchuDev <leshchudev@gmail.com>
2024-07-10 19:45:59 +0200

    connecting inputs with liesteners using arrow  funcions instead of bind() metod

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
@@ -13,10 +16,30 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input'
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  // uywam funkcji strzałkowej zeby nie musiec uzywac metody bind() do powiazania linii 41 z ta metoda
+  private submitHandler = (event: Event) => {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  };
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,23 @@
 "use strict";
 class ProjectInput {
     constructor() {
+        this.submitHandler = (event) => {
+            event.preventDefault();
+            console.log(this.titleInputElement.value);
+        };
         this.templateElement = document.getElementById("project-input");
         this.hostElement = document.getElementById("app");
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.element.id = 'user-input';
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
         this.attach();
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);

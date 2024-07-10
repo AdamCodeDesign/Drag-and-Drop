009a9c325a4dd7645b5cb028d2f0bdc15071f9f5
LeshchuDev <leshchudev@gmail.com>
2024-07-10 20:32:23 +0200

    submit form added and clear all inputs after it

--- app.ts
+++ app.ts
@@ -31,10 +31,38 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enterTitle = this.titleInputElement.value;
+    const enterDescription = this.descriptionInputElement.value;
+    const enterPeople = this.peopleInputElement.value;
+
+    if (
+      enterTitle.trim().length === 0 ||
+      enterDescription.trim().length === 0 ||
+      enterPeople.trim().length === 0
+    ) {
+      alert("Incorrect input, please try again!");
+      return;
+    } else {
+      return [enterTitle, enterDescription, +enterPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   // uywam funkcji strzałkowej zeby nie musiec uzywac metody bind() do powiazania linii 41 z ta metoda
   private submitHandler = (event: Event) => {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   };
 
   private configure() {
--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,12 @@ class ProjectInput {
     constructor() {
         this.submitHandler = (event) => {
             event.preventDefault();
-            console.log(this.titleInputElement.value);
+            const userInput = this.gatherUserInput();
+            if (Array.isArray(userInput)) {
+                const [title, desc, people] = userInput;
+                console.log(title, desc, people);
+                this.clearInputs();
+            }
         };
         this.templateElement = document.getElementById("project-input");
         this.hostElement = document.getElementById("app");
@@ -15,6 +20,25 @@ class ProjectInput {
         this.peopleInputElement = this.element.querySelector("#people");
         this.configure();
         this.attach();
+    }
+    gatherUserInput() {
+        const enterTitle = this.titleInputElement.value;
+        const enterDescription = this.descriptionInputElement.value;
+        const enterPeople = this.peopleInputElement.value;
+        if (enterTitle.trim().length === 0 ||
+            enterDescription.trim().length === 0 ||
+            enterPeople.trim().length === 0) {
+            alert("Incorrect input, please try again!");
+            return;
+        }
+        else {
+            return [enterTitle, enterDescription, +enterPeople];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
     }
     configure() {
         this.element.addEventListener("submit", this.submitHandler);
